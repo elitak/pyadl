a35e261e023731f4a1d906c81f24649bccc4c369
eric <elitak@gmail.com>
2011-06-20 18:14:20 -0700

    amdovdrvctrl submodule added + basic adl.cpp usage

--- pyadl.cpp
+++ pyadl.cpp
@@ -1,8 +1,10 @@
 #include <boost/python/module.hpp>
 #include <boost/python/class.hpp>
 #include "adl_sdk.h"
+#include "adl.h"
 
 using namespace boost::python;
+using boost::noncopyable;
  
 BOOST_PYTHON_MODULE(pyadl)
 {
@@ -15,6 +17,10 @@ BOOST_PYTHON_MODULE(pyadl)
       .def_readwrite("iMemoryClock", &ADLODPerformanceLevel::iMemoryClock)
       .def_readwrite("iVddc", &ADLODPerformanceLevel::iVddc)
    ;
-
+   class_<ADL, noncopyable>("ADL", no_init)
+      .def("Instance", ADL::Instance, return_value_policy<reference_existing_object>())
+      .staticmethod("Instance")
+      .def("IsATICardAndCatalystPresent", &ADL::IsATICardAndCatalystPresent)
+   ;
 }
 
