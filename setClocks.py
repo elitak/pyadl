3cd935e6ecf3761a42cd1965f916473ca07dda39
Debian Live user <user@localhost.localdomain>
2011-07-11 17:11:33 -0700

    moved clocks config into .yml

--- setClocks.py
+++ setClocks.py
@@ -3,33 +3,37 @@ from __future__ import print_function
 import pyadl
 import os
 import sys
+import yaml
+import re
 
 def main(argv=None):
    argv = argv or sys.argv
+   progDir = os.path.abspath(os.path.dirname(argv[0]))
+   confFile = re.sub(r'^(.*?)(\.py)?$', r'\1.yml', os.path.basename(argv[0]))
 
-   # guess the display, if unset 
+   config = yaml.load(open(os.path.join(progDir, confFile)))
+
+   # guess display if not set
    if 'DISPLAY' not in os.environ:
-      os.putenv('DISPLAY', ':0')
+      os.putenv('DISPLAY', config.setdefault('DISPLAY', ':0'))
+   # override if set in config
+   elif 'DISPLAY' in config:
+      os.putenv('DISPLAY', config['DISPLAY'])
 
-   raw_input('WARNING: DO NOT run this script without first editing '
-             'the overclocking configuration contained within it. '
-             'Press Ctrl-C to abort, enter to continue.')
+   if config.setdefault('warn', True):
+      raw_input(('WARNING: DO NOT run this script without first editing '
+                'the overclocking configuration in "{0}". '
+                'Press Ctrl-C to abort, enter to continue.').format(confFile))
 
    # TODO: detect model name and active adapters; use .yml config
 
-   config = {
-      # cardNdx: [EngineClock, MemClock, Vddc]
-      # The unit used for clock values here is hundredths of MHz.
-      # Zero-values are skipped rather than set.
-      0: [95000,10000,0],
-      4: [92000,10000,0],
-      8: [92000,10000,0],
-   }
+   factors = [100, 100, 1000]
+   gpuConfig = dict( (k, [int(v[i] * factor) for i, factor in enumerate(factors)]) for k, v in config['clocks'].items() )
 
    pyadl.ADL.Instance()
    raw_input('Look above and ensure you\'re configuring the active cards! Then, press enter.')
 
-   for gpuNdx, vals in config.iteritems():
+   for gpuNdx, vals in gpuConfig.iteritems():
       adapter = pyadl.Adapter(gpuNdx)
       levels = adapter.getPerformanceLevels()
       [ 
